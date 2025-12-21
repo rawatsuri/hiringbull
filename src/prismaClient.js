85bf60c8b054195f986db5d43c79205ba48d3e84
kabeerx9 <kabeer786joshi@gmail.com>
2025-12-21 16:34:05 +0530

    temp

--- src/prismaClient.js
+++ src/prismaClient.js
@@ -1,5 +1,13 @@
+// src/lib/prisma.js or wherever you initialize Prisma
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+  log: ['query', 'error', 'warn'],
+});
 
 export default prisma;
